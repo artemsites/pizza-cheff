0c0a39ae1228dd61e23411fe7ad67f26b7a94ca0
Artem Kuznecov <artem.kuznecov.samara@yandex.ru>
2023-07-27 00:39:17 +0400

    scroll smoothly by hash

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,3 +3,27 @@ import './_nav.js';
 import './_form.js';
 
 import './libs/fslightbox.js';
+
+import {scrollSmoothlyToPosition} from './libs/scrollSmoothlyToPosition.js';
+
+document.addEventListener('DOMContentLoaded', function () {
+  try {
+
+    let links = document.querySelectorAll('a');
+
+    links.forEach(function(link) {
+      link.addEventListener("click",function(e) {
+        e.preventDefault();
+        new scrollSmoothlyToPosition({
+          selectorTarget: link.hash,
+          marginTop: 50,// в "px"
+          // или в "rem"
+          // marginTop: "10rem",// необходимо чтобы у html (:root) был установлен font-size
+        });
+      });
+    });
+
+  } catch (err) {
+    console.error('TRY-CATCH ERROR: ', err)
+  } finally {}
+})
