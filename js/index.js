3d853feadd3c7b715878971a7dd5efbec22291dc
Artem Kuznecov <artem.kuznecov.samara@yandex.ru>
2023-07-26 22:51:25 +0400

    wow.js

--- js/index.js
+++ js/index.js
@@ -1,1 +1,18 @@
 import './_nav.js';
+
+document.addEventListener('DOMContentLoaded', function () {
+  try {
+
+
+
+    new WOW({
+      // live: false,
+      // offset: 50
+    }).init();
+
+
+
+  } catch (err) {
+    console.error('TRY-CATCH ERROR: ', err)
+  } finally {}
+})
